8b94d4e341e0d013fb2b6f7dca31661985df32b9
shubham <shubham992284@gmail.com>
2024-02-27 17:26:44 +0530

    header created with bootstrap

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link,NavLink} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>header</div>
-  )
-}
+<Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <NavLink to={'/'} style={{paddingLeft: 13, textDecoration: 'none'}}><Navbar.Brand href="#home">Fullstck App</Navbar.Brand></NavLink>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to={'/additem'} style={{ textDecoration: 'none'}}> <Nav.Link href="#link">Additem</Nav.Link></NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  )}
